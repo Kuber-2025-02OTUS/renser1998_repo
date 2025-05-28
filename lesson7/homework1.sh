ddad0bf50ed6d3c3615c7a25988d34ab30e68332
renser1998 <renser1998@yandex.ru>
2025-05-29 00:00:26 +0300

    7 lesson (1 and 2 parts)

---
+++ lesson7/homework1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+kubectl delete -f mysql-instance.yaml
+kubectl delete -f crd-op-deployment.yaml
+kubectl apply -f sa-cr-crb-crd.yaml
+kubectl apply -f crd-op-deployment.yaml
+kubectl apply -f mysql-instance.yaml
+echo "Resources removed and recreated successfully."
