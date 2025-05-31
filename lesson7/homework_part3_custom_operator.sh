c13cbc220d4c8cf63bcad07fd344af2e425988c8
renser1998 <renser1998@yandex.ru>
2025-05-31 23:11:01 +0300

    lesson 7_1

--- lesson7/homework_part3_custom_operator.sh
+++ lesson7/homework_part3_custom_operator.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 kubectl delete -f mysql-instance.yaml
 kubectl delete -f crd-op-deployment.yaml
-kubectl apply -f sa-cr-crb-crd.yaml
-kubectl apply -f crd-op-deployment.yaml
+
+kubectl apply -f crd.yaml
 kubectl apply -f mysql-instance.yaml
+python3 -m venv venv 
+source 
 echo "Resources removed and recreated successfully."
